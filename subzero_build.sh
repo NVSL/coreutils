e98298a501c27bf4c920ab4f240ef960901eece1
Yun Joon Soh <yjsoh@eng.ucsd.edu>
2019-08-16 14:22:05 -0700

    Updated subzero_build.sh for compatility with libsubzero

--- subzero_build.sh
+++ subzero_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export FORCE_UNSAFE_CONFIGURE=1
-./bootstrap
-CPPFLAGS="-O3 -lsubzero -L/usr/local/lib " CFLAGS="-O3 -lsubzero -L/usr/local/lib " CXXFLAGS="-O3 -lsubzero -L/usr/local/lib " LDFLAGS="-L/usr/local/lib" ./configure --disable-gcc-warnings
+#./bootstrap
+CPPFLAGS="-O3 -L/usr/local/lib64 -lpmem -lsubzero -L/usr/local/lib " CFLAGS="-O3 -L/usr/local/lib64 -lpmem -lsubzero -L/usr/local/lib " CXXFLAGS="-O3 -L/usr/local/lib64 -lpmem -lsubzero -L/usr/local/lib " LDFLAGS="-L/usr/local/lib64 -L/usr/local/lib" ./configure --disable-gcc-warnings
 make -j48
